8232a8a6f98c5a259a7b2c789cbf5e075494be6d
Ryedla <rama.thursday@gmail.com>
2019-11-25 05:56:21 +0000

    added Rmd file and changed script

--- A lot about R.R
+++ A lot about R.R
@@ -1,12 +1,25 @@
-#R datasets can be explored by using...
+                          #R datasets can be explored by using...
 datasets::
   
 #Looking for the help on datasets?
 library(help = 'datasets')
 
-###Vector Math###
-many.numbers <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
-many.numbers <- c(1:9)
-two.times <- many.numbers*2
-more.numbers <- c(2, 4, 5, 7, 3, 5, 9, 7, 1)
-many.numbers + more.numbers
+                          ###Vector Math###
+numbers1 <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+numbers1 <- c(1:9)
+two.numbers <- many.numbers*2
+numbers2 <- c(2, 4, 5, 7, 3, 5, 9, 7, 1)
+numbers1 + numbers2
+
+"What happens if we try to do math with two vectors of different length? 
+Let's see that below"
+
+less_numbers <- c(2, 3)
+less_numbers + numbers1
+
+"The process of recycling comes into picture. The shorter vector will be
+used on a longer vector multiple times and this is the same in case of 
+other operations"
+
+
+
