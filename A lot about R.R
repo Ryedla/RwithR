45442ab027cef4fe1128886f97456c991ff8b5df
Ryedla <rama.thursday@gmail.com>
2019-11-25 05:41:53 +0000

    Changed a few files

---
+++ A lot about R.R
@@ -0,0 +1,12 @@
+#R datasets can be explored by using...
+datasets::
+  
+#Looking for the help on datasets?
+library(help = 'datasets')
+
+###Vector Math###
+many.numbers <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+many.numbers <- c(1:9)
+two.times <- many.numbers*2
+more.numbers <- c(2, 4, 5, 7, 3, 5, 9, 7, 1)
+many.numbers + more.numbers
